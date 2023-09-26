436596a76acca8afc1779880e643c9368f7050d7
Miguel Rivas <mrivaslezama@gmail.com>
2023-09-25 22:53:53 -0300

    inicio config github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()]
-});
+  site: 'https://mrivaslezama.github.io/formatoinicial/',
+  base: '/formatoinicial',
+})
